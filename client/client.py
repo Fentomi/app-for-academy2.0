cf247b5172a213f611c17e7817edc18c61176a4e
Nikita <105577819+Fentomi@users.noreply.github.com>
2023-08-29 21:47:02 +0300

    refactoring full project

--- client/client.py
+++ client/client.py
@@ -1,22 +1,34 @@
 import socket
+import pickle
 from server_config import SERVER_IP, SERVER_PORT
 
 class Client():
-    def __init__(self):
-        try:
-            self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.client.connect((SERVER_IP, SERVER_PORT))
-            print('Connect with server [ACCESS]')
-        except:
-            print('Connect with server [DENIED]')
+    #connect with server
+    def connect(self, ip_server: str, port_server: int):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((ip_server, port_server))
 
-    def send_message_on_server(self, text: str) -> None:
-        self.client.send(text.encode('utf-8'))
+    #send and recv sql-request on server
+    def send_and_recv_request_on_server(self, sql_request: str) -> (tuple, bool):
+        #send request on server
+        self.client.send(pickle.dumps(sql_request))
+        #get result with server
+        self.data = pickle.loads(self.client.recv(1024))
+        if self.data is not None:
+            return self.data
+        return False
+
+    #close connection with server
+    def close(self):
+        self.client.close()
 
 
 def main():
     client = Client()
-    client.send_message_on_server('Example')
+    client.connect(SERVER_IP, SERVER_PORT)
+    answer = client.send_and_recv_request_on_server('Privet!')
+    print(answer)
+    client.close()
 
 
 if __name__ == '__main__':
--- server/server.py
+++ server/server.py
@@ -1,33 +1,53 @@
 from server_config import SERVER_IP, SERVER_PORT
 import socket
+import pickle
 
 
 class Server():
-    def __init__(self):
+    #create server
+    def create(self, ip_server: str, port_server: int) -> None:
         try:
             self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.server.bind((SERVER_IP, SERVER_PORT))
-            print('Create server [ACCESS]')
+            self.server.bind((ip_server, port_server))
+            print('Server created [ACCESS]')
+            print('~' * 30)
         except:
-            print('Create server [DENIED]')
+            print('Server created [DENIED]')
 
-    def open_server(self):
+    #open server for connections and transactions data
+    def open(self) -> None:
         self.server.listen()
-        print('Server is listen...')
-        while True:
-            self.user, self.data = self.server.accept()
-            print(f'Server find a friend! it\'s {self.user}')
-            self.get_request_from_client()
 
-    def get_request_from_client(self):
-        request = self.user.recv(10000).decode('utf-8')
-        print(request)
+        while True:
+            #client connected
+            conn, addr = self.server.accept()
+            print(f'{addr[0]} connected.')
+
+            #load sql-request
+            sql_request = pickle.loads(conn.recv(1024))
+            print(f'sql_request: {sql_request}')
+
+            #send sql-request in database and get data
+            data = self.send_request_in_database(sql_request)
+            print(f'data is: {data}')
+
+            #send client data
+            conn.send(pickle.dumps(data))
+
+            #close connection with client
+            conn.close()
+            print(f'connection close')
+            print('~' * 30)
+
+    #send sql-request on database and return tuple or bool result
+    def send_request_in_database(self, sql_request: str) -> (bool, tuple):
+        return [('haha')]
 
 
 def main():
     server = Server()
-    server.open_server()
-
+    server.create(SERVER_IP, SERVER_PORT)
+    server.open()
 
 if __name__ == '__main__':
     main()
