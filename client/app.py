bd175e700389af288ed234a4417f9659b001d1f2
Fentomi <105577819+Fentomi@users.noreply.github.com>
2023-08-31 00:46:36 +0300

    add table after skidka

--- client/app.py
+++ client/app.py
@@ -49,7 +49,12 @@ class Application(QMainWindow):
         data = client.send_and_recv_request_on_server(f"select * from learntype where learntype='{combobox}'")
         client.close()
 
-        self.fill_tablewidget(self.ui.tableWidget_before, data)
+        if skidka == '':
+            self.fill_tablewidget(self.ui.tableWidget_before, data)
+        else:
+            self.fill_tablewidget(self.ui.tableWidget_before, data)
+            self.fill_tablewidget(self.ui.tableWidget_after, self.change_data_with_skidka(data, skidka))
+
     def clear_button_search(self):
         client = Client()
         client.connect()
@@ -57,6 +62,23 @@ class Application(QMainWindow):
         client.close()
 
         self.fill_tablewidget(self.ui.tableWidget_before, data)
+        self.ui.tableWidget_after.clearContents()
+        self.ui.tableWidget_after.setRowCount(0)
+
+    def change_data_with_skidka(self, tuple_data, skidka=0):
+        change_data = []
+        float_skidka = float(f'0.{skidka}')
+        for i in range(len(tuple_data)):
+            list_data = list(tuple_data[i])
+            for j in range(len(tuple_data[i])):
+                if type(tuple_data[i][j]) is int and j != 2:
+                    if skidka == 0:
+                        list_data[j] = int(tuple_data[i][j])
+                    else:
+                        list_data[j] = int(tuple_data[i][j] - (tuple_data[i][j] * float_skidka))
+            change_data.append(tuple(list_data))
+        return change_data
+
 
 
 if __name__ == '__main__':
