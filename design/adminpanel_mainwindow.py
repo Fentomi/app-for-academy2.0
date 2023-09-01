eeb2c8e695a50a335ff06fdd4a65350d7b9e87ce
Fentomi <105577819+Fentomi@users.noreply.github.com>
2023-09-01 10:21:01 +0300

    change design adminpanel

--- design/adminpanel_mainwindow.py
+++ design/adminpanel_mainwindow.py
@@ -19,12 +19,14 @@ from PySide6.QtWidgets import (QApplication, QLabel, QLineEdit, QMainWindow,
     QPushButton, QSizePolicy, QWidget)
 
 class Ui_adminpanel(object):
-    def setupUi(self, MainWindow):
-        if not MainWindow.objectName():
-            MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(400, 300)
-        MainWindow.setStyleSheet(u"background-color: rgb(32,37,43);")
-        self.centralwidget = QWidget(MainWindow)
+    def setupUi(self, FentomiApp):
+        if not FentomiApp.objectName():
+            FentomiApp.setObjectName(u"FentomiApp")
+        FentomiApp.resize(400, 300)
+        FentomiApp.setMinimumSize(QSize(400, 300))
+        FentomiApp.setMaximumSize(QSize(400, 300))
+        FentomiApp.setStyleSheet(u"background-color: rgb(32,37,43);")
+        self.centralwidget = QWidget(FentomiApp)
         self.centralwidget.setObjectName(u"centralwidget")
         self.label = QLabel(self.centralwidget)
         self.label.setObjectName(u"label")
@@ -66,27 +68,16 @@ class Ui_adminpanel(object):
 "color: white;\n"
 "margin: 0 auto;\n"
 "font-size: 20px;")
-        self.lineedit_learntype = QLineEdit(self.centralwidget)
-        self.lineedit_learntype.setObjectName(u"lineedit_learntype")
-        self.lineedit_learntype.setGeometry(QRect(220, 60, 151, 21))
-        self.lineedit_learntype.setStyleSheet(u"background-color: rgba(43,48,55, 0.8);\n"
-"border: 1px solid gray;\n"
-"border-radius: 5px;\n"
-"color: white;")
-        self.lineedit_learnprice_year = QLineEdit(self.centralwidget)
-        self.lineedit_learnprice_year.setObjectName(u"lineedit_learnprice_year")
-        self.lineedit_learnprice_year.setGeometry(QRect(220, 180, 151, 21))
-        self.lineedit_learnprice_year.setStyleSheet(u"background-color: rgba(43,48,55, 0.8);\n"
-"border: 1px solid gray;\n"
-"border-radius: 5px;\n"
-"color: white;")
         self.label_3 = QLabel(self.centralwidget)
         self.label_3.setObjectName(u"label_3")
-        self.label_3.setGeometry(QRect(50, 60, 261, 21))
+        self.label_3.setGeometry(QRect(50, 60, 191, 21))
         self.label_3.setStyleSheet(u"background-color: none;\n"
 "color: white;\n"
 "margin: 0 auto;\n"
 "font-size: 14px;")
+        self.label_3.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
+        self.label_3.setWordWrap(False)
+        self.label_3.setIndent(0)
         self.pushButton_delete = QPushButton(self.centralwidget)
         self.pushButton_delete.setObjectName(u"pushButton_delete")
         self.pushButton_delete.setGeometry(QRect(260, 250, 75, 24))
@@ -94,17 +85,31 @@ class Ui_adminpanel(object):
 "border: 1px solid gray;\n"
 "border-radius: 5px;\n"
 "color: white;")
-        self.lineedit_learntime = QLineEdit(self.centralwidget)
-        self.lineedit_learntime.setObjectName(u"lineedit_learntime")
-        self.lineedit_learntime.setGeometry(QRect(220, 90, 151, 21))
-        self.lineedit_learntime.setStyleSheet(u"background-color: rgba(43,48,55, 0.8);\n"
-"border: 1px solid gray;\n"
-"border-radius: 5px;\n"
-"color: white;")
         self.pushButton_change = QPushButton(self.centralwidget)
         self.pushButton_change.setObjectName(u"pushButton_change")
         self.pushButton_change.setGeometry(QRect(160, 250, 75, 24))
         self.pushButton_change.setStyleSheet(u"background-color: rgba(43,48,55, 0.8);\n"
+"border: 1px solid gray;\n"
+"border-radius: 5px;\n"
+"color: white;")
+        self.label_4 = QLabel(self.centralwidget)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setGeometry(QRect(30, 90, 261, 21))
+        self.label_4.setStyleSheet(u"background-color: none;\n"
+"color: white;\n"
+"margin: 0 auto;\n"
+"font-size: 14px;")
+        self.lineedit_learntype = QLineEdit(self.centralwidget)
+        self.lineedit_learntype.setObjectName(u"lineedit_learntype")
+        self.lineedit_learntype.setGeometry(QRect(220, 60, 151, 21))
+        self.lineedit_learntype.setStyleSheet(u"background-color: rgba(43,48,55, 0.8);\n"
+"border: 1px solid gray;\n"
+"border-radius: 5px;\n"
+"color: white;")
+        self.lineedit_learntime = QLineEdit(self.centralwidget)
+        self.lineedit_learntime.setObjectName(u"lineedit_learntime")
+        self.lineedit_learntime.setGeometry(QRect(220, 90, 151, 21))
+        self.lineedit_learntime.setStyleSheet(u"background-color: rgba(43,48,55, 0.8);\n"
 "border: 1px solid gray;\n"
 "border-radius: 5px;\n"
 "color: white;")
@@ -122,31 +127,31 @@ class Ui_adminpanel(object):
 "border: 1px solid gray;\n"
 "border-radius: 5px;\n"
 "color: white;")
-        self.label_4 = QLabel(self.centralwidget)
-        self.label_4.setObjectName(u"label_4")
-        self.label_4.setGeometry(QRect(30, 90, 261, 21))
-        self.label_4.setStyleSheet(u"background-color: none;\n"
-"color: white;\n"
-"margin: 0 auto;\n"
-"font-size: 14px;")
-        MainWindow.setCentralWidget(self.centralwidget)
+        self.lineedit_learnprice_year = QLineEdit(self.centralwidget)
+        self.lineedit_learnprice_year.setObjectName(u"lineedit_learnprice_year")
+        self.lineedit_learnprice_year.setGeometry(QRect(220, 180, 151, 21))
+        self.lineedit_learnprice_year.setStyleSheet(u"background-color: rgba(43,48,55, 0.8);\n"
+"border: 1px solid gray;\n"
+"border-radius: 5px;\n"
+"color: white;")
+        FentomiApp.setCentralWidget(self.centralwidget)
 
-        self.retranslateUi(MainWindow)
+        self.retranslateUi(FentomiApp)
 
-        QMetaObject.connectSlotsByName(MainWindow)
+        QMetaObject.connectSlotsByName(FentomiApp)
     # setupUi
 
-    def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+    def retranslateUi(self, FentomiApp):
+        FentomiApp.setWindowTitle(QCoreApplication.translate("FentomiApp", u"MainWindow", None))
         self.label.setText("")
-        self.label_6.setText(QCoreApplication.translate("MainWindow", u"\u0421\u0442\u043e\u0438\u043c\u043e\u0441\u0442\u044c \u043e\u0431\u0443\u0447\u0435\u043d\u0438\u044f(\u0437\u0430 \u0441\u0435\u043c.):", None))
-        self.pushButton_add.setText(QCoreApplication.translate("MainWindow", u"\u0414\u043e\u0431\u0430\u0432\u0438\u0442\u044c", None))
-        self.label_7.setText(QCoreApplication.translate("MainWindow", u"\u0421\u0442\u043e\u0438\u043c\u043e\u0441\u0442\u044c \u043e\u0431\u0443\u0447\u0435\u043d\u0438\u044f (\u0437\u0430 \u0433\u043e\u0434):", None))
-        self.label_5.setText(QCoreApplication.translate("MainWindow", u"\u0421\u0442\u043e\u0438\u043c\u043e\u0441\u0442\u044c \u043e\u0431\u0443\u0447\u0435\u043d\u0438\u044f(\u0437\u0430 \u043c\u0435\u0441.):", None))
-        self.label_2.setText(QCoreApplication.translate("MainWindow", u"\u0414\u043e\u0431\u0430\u0432\u0438\u0442\u044c, \u0438\u0437\u043c\u0435\u043d\u0438\u0442\u044c, \u0443\u0434\u0430\u043b\u0438\u0442\u044c \u0444\u043e\u0440\u043c\u0443", None))
-        self.label_3.setText(QCoreApplication.translate("MainWindow", u"\u0424\u043e\u0440\u043c\u0430 \u043e\u0431\u0443\u0447\u0435\u043d\u0438\u044f:", None))
-        self.pushButton_delete.setText(QCoreApplication.translate("MainWindow", u"\u0423\u0434\u0430\u043b\u0438\u0442\u044c", None))
-        self.pushButton_change.setText(QCoreApplication.translate("MainWindow", u"\u0418\u0437\u043c\u0435\u043d\u0438\u0442\u044c", None))
-        self.label_4.setText(QCoreApplication.translate("MainWindow", u"\u0421\u0440\u043e\u043a \u043e\u0431\u0443\u0447\u0435\u043d\u0438\u044f (\u043c\u0435\u0441):", None))
+        self.label_6.setText(QCoreApplication.translate("FentomiApp", u"\u0421\u0442\u043e\u0438\u043c\u043e\u0441\u0442\u044c \u043e\u0431\u0443\u0447\u0435\u043d\u0438\u044f(\u0437\u0430 \u0441\u0435\u043c.):", None))
+        self.pushButton_add.setText(QCoreApplication.translate("FentomiApp", u"\u0414\u043e\u0431\u0430\u0432\u0438\u0442\u044c", None))
+        self.label_7.setText(QCoreApplication.translate("FentomiApp", u"\u0421\u0442\u043e\u0438\u043c\u043e\u0441\u0442\u044c \u043e\u0431\u0443\u0447\u0435\u043d\u0438\u044f (\u0437\u0430 \u0433\u043e\u0434):", None))
+        self.label_5.setText(QCoreApplication.translate("FentomiApp", u"\u0421\u0442\u043e\u0438\u043c\u043e\u0441\u0442\u044c \u043e\u0431\u0443\u0447\u0435\u043d\u0438\u044f(\u0437\u0430 \u043c\u0435\u0441.):", None))
+        self.label_2.setText(QCoreApplication.translate("FentomiApp", u"\u0414\u043e\u0431\u0430\u0432\u0438\u0442\u044c, \u0438\u0437\u043c\u0435\u043d\u0438\u0442\u044c, \u0443\u0434\u0430\u043b\u0438\u0442\u044c \u0444\u043e\u0440\u043c\u0443", None))
+        self.label_3.setText(QCoreApplication.translate("FentomiApp", u"\u0424\u043e\u0440\u043c\u0430 \u043e\u0431\u0443\u0447\u0435\u043d\u0438\u044f:", None))
+        self.pushButton_delete.setText(QCoreApplication.translate("FentomiApp", u"\u0423\u0434\u0430\u043b\u0438\u0442\u044c", None))
+        self.pushButton_change.setText(QCoreApplication.translate("FentomiApp", u"\u0418\u0437\u043c\u0435\u043d\u0438\u0442\u044c", None))
+        self.label_4.setText(QCoreApplication.translate("FentomiApp", u"\u0421\u0440\u043e\u043a \u043e\u0431\u0443\u0447\u0435\u043d\u0438\u044f (\u043c\u0435\u0441):", None))
     # retranslateUi
 
